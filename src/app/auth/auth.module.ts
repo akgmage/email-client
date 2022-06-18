482cdea38ab22ed663e39885dbf3adedbcca02af
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-18 22:32:59 +0530

    Add Signin and Signup component inside Auth module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SigninComponent,
+    SignupComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
