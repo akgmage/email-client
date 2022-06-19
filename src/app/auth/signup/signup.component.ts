594e8b69bf578cdda906d5c16f5e1b6416a36189
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-19 12:14:03 +0530

    Wire up custom validator

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatchPassword } from '../validators/match-password';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -24,9 +25,10 @@ export class SignupComponent implements OnInit {
         Validators.minLength(4),
         Validators.maxLength(20)
       ]),
-  });
+  }, { validators: [this.matchPassword.validate] }
+  );
   
-  constructor() { }
+  constructor(private matchPassword: MatchPassword) { }
 
   ngOnInit(): void {
   }
