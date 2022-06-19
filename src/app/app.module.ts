fd3d8dcf744319c4395a7d118514c3a8f8d9f58e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-19 11:15:23 +0530

    1. Add Input module inside Shared Module 2. Add configuration rules to routes array 3. Import Auth module in App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AuthModule } from './auth/auth.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
