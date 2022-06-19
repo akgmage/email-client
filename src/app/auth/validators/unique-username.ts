3bdc7c0aaaed8de925e53db58024e0d89091e9a1
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-19 20:51:42 +0530

    Build AuthService to check availability of username

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,19 +1,17 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
 import { AbstractControl, AsyncValidator, FormControl } from "@angular/forms";
 import { map, catchError } from "rxjs/operators";
 import { of } from "rxjs";
-
+import { AuthService } from "../auth.service";
 @Injectable({providedIn: 'root'})
 export class UniqueUsername implements AsyncValidator{
-    constructor(private http: HttpClient) {
+    constructor(private authService:AuthService) {
         
     }
     validate = (control: AbstractControl) => {
         const { value } = control;
-        return this.http.post<any>('https://api.angular-email.com/auth/username', {
-            username: value
-        }).pipe(
+        return this.authService.usernameAvailable(value)
+        .pipe(
             map(value => {
                 return null;
             }),
