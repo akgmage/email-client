a993b5bb8bd87ff8c93878b53369a7dacfb4abf0
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-19 21:02:22 +0530

    Add Reusable Input

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [
     SigninComponent,
@@ -13,7 +13,8 @@ import { SignupComponent } from './signup/signup.component';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
