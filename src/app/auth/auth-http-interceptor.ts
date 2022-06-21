e3f2bfe26aacada916e7fa741ba5088d5bbdc8e0
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-21 23:44:20 +0530

    Work with outgoing request and modify it, take a look at response or any different events tied to the response

--- src/app/auth/auth-http-interceptor.ts
+++ src/app/auth/auth-http-interceptor.ts
@@ -3,8 +3,10 @@ import {
     HttpEvent,
     HttpInterceptor,
     HttpHandler,
-    HttpRequest
+    HttpRequest,
+    HttpEventType
 } from '@angular/common/http';
+import { filter, tap } from "rxjs";
 import { Observable } from "rxjs";
 @Injectable()
 export class AuthHttpInterceptor implements HttpInterceptor{
@@ -12,9 +14,16 @@ export class AuthHttpInterceptor implements HttpInterceptor{
         req: HttpRequest<any>, 
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
+        // MOdify or log the outgoing request
         const modifiedReq = req.clone({
             withCredentials: true,
         });
-        return next.handle(modifiedReq);
+        return next.handle(modifiedReq)
+            .pipe(
+                filter(val => val.type === HttpEventType.Sent),
+                tap(val => {
+                    console.log('Sent the request')
+                })
+            );
     }
 }
