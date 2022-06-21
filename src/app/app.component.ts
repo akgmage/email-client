92515be39fe83b1fcfa0e7b572a4df8e09aad970
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-21 22:37:31 +0530

    Save a refrence to the observable and referance that inside template using async operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { AuthService } from './auth/auth.service';
 @Component({
   selector: 'app-root',
@@ -6,13 +7,8 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  signedin = false;
+  signedin$: BehaviorSubject<boolean>;
   constructor(private authService: AuthService) {
-
-  }
-  ngOnInit() {
-    this.authService.signedin$.subscribe((signedin) => {
-      this.signedin = signedin;
-    });
+    this.signedin$ = this.authService.signedin$;
   }
 }
