eb07199d26956ac6d18808c0398d4ce6c5ee878d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-22 22:58:42 +0530

    Navigate to root url on signout

--- src/app/auth/signout/signout.component.ts
+++ src/app/auth/signout/signout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-signout',
   templateUrl: './signout.component.html',
@@ -7,11 +8,14 @@ import { AuthService } from '../auth.service';
 })
 export class SignoutComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.authService.signout().subscribe(() => {
-      
+      this.router.navigateByUrl('/');
     });
   }
 
