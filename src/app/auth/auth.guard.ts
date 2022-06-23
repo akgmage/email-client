45f87cd7a9b8a8d2e95e32d1f1598c2253a1cda4
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-23 22:50:50 +0530

    Fix auth guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
-
+import { take, skipWhile} from 'rxjs/operators';
+import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private authService:AuthService) {
+
+  }
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
@@ -14,6 +18,9 @@ export class AuthGuard implements CanActivate, CanLoad {
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return false;
+    return this.authService.signedin$.pipe(
+      skipWhile(value => value === null),
+      take(1),
+    );
   }
 }
