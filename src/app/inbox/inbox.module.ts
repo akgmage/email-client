1b26f9fb71af4438e519ad4fdbfbfa2682cb6af4
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-23 23:00:58 +0530

    Add EmailIndex component inside inbox module

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -5,13 +5,15 @@ import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
 import { EmailCreateComponent } from './email-create/email-create.component';
 import { EmailReplyComponent } from './email-reply/email-reply.component';
+import { EmailIndexComponent } from './email-index/email-index.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     EmailCreateComponent,
-    EmailReplyComponent
+    EmailReplyComponent,
+    EmailIndexComponent
   ],
   imports: [
     CommonModule,
