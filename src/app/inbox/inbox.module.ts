d81455aea62bacef9760fc2d18bab266834a543d
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-06-23 23:00:38 +0530

    Add EmailReply component inside inbox module

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
 import { EmailCreateComponent } from './email-create/email-create.component';
+import { EmailReplyComponent } from './email-reply/email-reply.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    EmailCreateComponent
+    EmailCreateComponent,
+    EmailReplyComponent
   ],
   imports: [
     CommonModule,
